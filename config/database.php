f9c020ec9d0fb44e2dd0f8ac49ab0be7bac51ffe
Zachriss <Zachriss@users.noreply.github.com>
2026-07-11 16:26:17 +0300

    Fix: Add openssl, improve SSL options with verify_server_cert=false

--- config/database.php
+++ config/database.php
@@ -48,6 +48,9 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/ca-certificates.crt'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
+                PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY'),
+                PDO::MYSQL_ATTR_SSL_CERT => env('MYSQL_ATTR_SSL_CERT'),
             ]) : [],
         ],
 
